63a390e3a88495b7174e7535c0767fd2f22fb280
Alberto <albertopastrolin@gmail.com>
2021-03-06 17:01:42 +0100

    Test

--- extend.php
+++ extend.php
@@ -11,26 +11,98 @@
 
 namespace Isamuraii\Emoticons;
 
-use Flarum\Extend;
-use s9e\TextFormatter\Configurator;
-
 return [
-  (new Extend\Frontend('forum'))
-    ->css(__DIR__.'/less/forum.less'),
-
   (new Extend\Formatter())
-    ->configure(function (Configurator $configurator) {
+   ->configure(function (Configurator $configurator) {
+     $emoticons = [
+       "+1",
+       "addit",
+       "ahumma",
+       "alone",
+       "asd",
+       "autopat",
+       "axe",
+       "bad",
+       "bah",
+       "boing",
+       "botte",
+       "bro",
+       "ç_ç",
+       "çAç",
+       "caffè",
+       "ciao",
+       "climb",
+       "crazy",
+       "cry",
+       "eee",
+       "ehooo",
+       "facepalm",
+       "fifi",
+       "filenfio",
+       "flame",
+       "flower",
+       "fofo",
+       "fyeah",
+       "gogo",
+       "gogo2",
+       "gogo3",
+       "gogo4",
+       "grattz",
+       "grrrr",
+       "grrrrrrr",
+       "hmm",
+       "hola",
+       "hold",
+       "holdskler",
+       "kill",
+       "L",
+       "look",
+       "malv",
+       "malvasd",
+       "master",
+       "mka",
+       "mki",
+       "ninja",
+       "NO",
+       "nono",
+       "o_o",
+       "ocb",
+       "OMG",
+       "party",
+       "patpat",
+       "pff",
+       "pip",
+       "popcorn",
+       "prrr",
+       "rotfl",
+       "sad",
+       "sasa",
+       "sbav",
+       "sbav2",
+       "scisci",
+       "sese",
+       "silook",
+       "sisi",
+       "skler",
+       "smile_drop",
+       "stalk",
+       "suicide",
+       "suicide2",
+       "superpat",
+       "susu",
+       "trollface",
+       "what",
+       "yeah",
+       "yeee",
+       "zizi"
+    ];
+    for($i = 0; $i < $emoticons.length; $i+=1) {
       $configurator->Emoticons->add(
-        ':sisi:', '<img class="emoticon" src="https://cdn.off-popic.it/emoticons/sisi.gif" alt=":sisi:" title="sisi">'
+        ":#{$emoticons[$i]}:",
+        "<img class=\"emoticon\" src=\"https://cdn.off-popic.it/$emoticons/#{$emoticons[$i]}.gif\" alt=\":#{$emoticons[$i]}:\" title=\":#{$emoticons[$i]}\">"
       );
-      $configurator->Emoticons->add(
-        ':asd:', '<img class="emoticon" src="https://cdn.off-popic.it/emoticons/asd.gif" alt=":asd:" title="asd">'
-      );
-      $configurator->Emoticons->add(
-        ':look:', '<img class="emoticon" src="https://cdn.off-popic.it/emoticons/look.gif" alt=":look:" title="look">'
-      );
-      $configurator->Emoticons->add(
-        ':rotfl:', '<img class="emoticon" src="https://cdn.off-popic.it/emoticons/rotfl.gif" alt=":rotfl:" title="rotfl">'
-      );
-    }),
+    }
+    $configurator->Emoticons->add(":test:", "Hello world");
+    $configurator->Emoticons->add(":debug:", $emoticons.join(";"));
+  })
 ];
